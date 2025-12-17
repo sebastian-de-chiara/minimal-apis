2fe45fa935376d5551f9bd9d9350ccaf0a3198fb
Sebastian De Chiara <sebastian.dechiara@protonmail.com>
2025-12-17 19:51:05 +1030

    Removed and sorted usings

--- minimal_apis/minimal_apis/Models/Service.cs
+++ minimal_apis/minimal_apis/Models/Service.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace minimal_apis.Models;
 
--- minimal_apis/minimal_apis/Program.cs
+++ minimal_apis/minimal_apis/Program.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Linq;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
